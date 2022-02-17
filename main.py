ee847346ce12022c91f8b3a3c3c78de20e9c2f18
Alberto Battistel <alberto.battistel@rub.de>
2022-02-17 22:21:32 +0100

    Update main.py
    It shows the screen.

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ import pygame
 
 pygame.font.init()
 
-screen_size = {'width':900, 
-               'height':500,
+screen_size = {'width':1280, 
+               'height':800,
                }
 
 FPS = 60
 
 WIN = pygame.display.set_mode((screen_size['width'], screen_size['height']))
 pygame.display.set_caption("First Game!")
+
+clock = pygame.time.Clock()
 
 run = True
 while run:
