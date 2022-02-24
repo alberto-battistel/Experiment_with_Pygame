4ee671bf2c4fee21364929ff43ecfd10978209fd
Alberto Battistel <alberto.battistel@rub.de>
2022-02-24 22:15:33 +0100

    FSM enter_state, in_state, exit_state modified
    Modification inside the class  enter_state, in_state, exit_state modified. I am not sure jet if this is the right direction.
    Just installed Spyder with a project into the folder

--- FSM.py
+++ FSM.py
@@ -1,9 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 17 2022
 
+@author: alberto
+"""
 
 class Finite_State_Machine:
     def __init__(self):
         pass
-    
     
     
 class Event_State_Table:
@@ -21,6 +26,7 @@ class Event:
     
 class State:
     just_entered = True
+    exit_signal = False
     
     def __init__(self, name):
         self.name = name
@@ -28,12 +34,21 @@ class State:
     def __str__(self):
         return self.name
         
-    def enter_state():
+    def enter_state(self):
         print('just entered')
         just_entered = False
+        return just_entered
         
-    def in_state():
+    def in_state(self):
         print('still in state')
         
-    def exit_state():
+    def exit_state(self):
         print('exit state')
+        
+    if just_entered:
+        enter_state()
+    else:
+        in_state()
+    
+    if exit_signal:
+        exit_state()
