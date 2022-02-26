22bfed426fba65cc887b6af0e4cd07e12e1aa104
Alberto Battistel <alberto.battistel@rub.de>
2022-02-26 23:02:38 +0100

    Half way through triggering the events
    Problems with checking the Finite_State_Machine check_table().

--- FSM.py
+++ FSM.py
@@ -10,38 +10,87 @@ from colorama import init, Fore, Back, Style
 init(autoreset=True)
 
 
-DEBUGMODE = False
+DEBUGMODE = True
 COLOR = Fore.MAGENTA
 
 def debug_print(string):
     if DEBUGMODE:
         print(COLOR + string) 
     
+def unpack_list(obj, elements):
+    if type(elements) == list:
+        for element in elements:
+            obj.append(element)
+    else:
+        obj.append(element)
+        
     
 class Finite_State_Machine:
     def __init__(self, FSM_name):
         self.name = FSM_name
         self.event_list = [] # so I can an Event as component?
+        self.state_list = [] # so I can an State as component?
+        self.event_state_table = None
+        self.actual_state = None
         
     def __str__(self):
         return self.name
     
+    def add_to_states(self, elements):
+        unpack_list(self.state_list, elements)
+        
+    def pass_starting_state(self):
+        for state in self.state_list:
+            debug_print(state.name)
+            if state.is_starting == True:
+                self.actual_state = str(state)
+                debug_print('Starting state: ' + str(state))
+    
+    def add_to_events(self, elements):
+        unpack_list(self.event_list, elements)
+    
     # def event_triggered(self, event):
     #     print('Triggered from ' + self.name + ' found: ' + '{:}'.format(event))
     
+    def print_state_list(self):
+        for state in self.state_list:
+            print(str(state))
+        print('')
+    
     def print_event_list(self):
         for event in self.event_list:
-            print(event.name)
+            print(str(event))
+        print('')
+        
+    # def check_events(self, pygame_event):
+    #     for event in self.event_list:
+    #         event.check_events(pygame_event)
+    #         if event.triggered:
+    #             debug_print('From ' + str(self) + ' found: ' + '{:}'.format(event))
+    #             event.triggered = False
     
     def check_events(self, pygame_event):
         for event in self.event_list:
             event.check_events(pygame_event)
             if event.triggered:
-                debug_print('From ' + self.name + ' found: ' + '{:}'.format(event))
+                debug_print('From ' + str(self) + ' found: ' + '{:}'.format(event))
                 event.triggered = False
     
-    def check_table(self, table):
-        pass
+    def check_table(self):
+        debug_print(self.actual_state)
+        for key,values in self.event_state_table.dictionary.items():
+            debug_print(key)
+            # I am somewhere here
+            if key == self.actual_state:
+                for event in self.event_list:
+                    if event.triggered:
+                        for value in values:
+                            if str(event) == value[0]: 
+                                debug_print(str(event) + 'moved from ' + self.actual_state)
+                                self.actual_state = value[1]            
+                    
+                    # if event.triggered:
+                    #     debug_print(str(event) + 'triggered passage of state')
                 
     
 class Event_State_Table:
@@ -53,6 +102,7 @@ class Event_State_Table:
             fmt = "State {}: " + (len(values)-1)*" {},"
             fmt += " {}"
             print(fmt.format(key, *values))
+        print('')
  
     
 class Event():
@@ -66,9 +116,7 @@ class Event():
                 self.event_list.append(event)         
         else:
             self.event_list = [event_list]
-            
-        self.debug = debug
-                        
+                                    
     def __str__(self):
         return self.name
             
@@ -90,8 +138,9 @@ class Event():
     
     
 class State:    
-    def __init__(self, name):
+    def __init__(self, name, is_starting=False):
         self.name = name
+        self.is_starting = is_starting
         self.just_entered = True
         self.exit_signal = False
         
@@ -99,14 +148,14 @@ class State:
         return self.name
         
     def enter_state(self):
-        print('just entered')
+        debug_print('Just entered in ' + self.name)
         self.just_entered = False
         
-    def in_state():
-        print('still in state')
+    def in_state(self):
+        debug_print('Still in '  + self.name)
         
-    def exit_state():
-        print('exit state')
+    def exit_state(self):
+        debug_print('Exit ' + self.name)
         
     def run_state(self):
         if self.just_entered:
--- FSM_test.py
+++ FSM_test.py
@@ -14,10 +14,8 @@ import FSM
 FSM_test = FSM.Finite_State_Machine('FSM_test')
 FSM_event_pressed_right = FSM.Event('pressed_right', [pygame.K_RIGHT])
 FSM_event_pressed_left = FSM.Event('pressed_left', [pygame.K_LEFT])
-# FSM_event.print_list()
 
-FSM_test.event_list.append(FSM_event_pressed_right)
-FSM_test.event_list.append(FSM_event_pressed_left)
+FSM_test.add_to_events([FSM_event_pressed_right, FSM_event_pressed_left])
 
 FSM_test.print_event_list()
 
@@ -25,6 +23,15 @@ dictionary = {'state1': [('pressed_right', 'state2')],
               'state2': [('pressed_left', 'state1')],}
 FSM_table = FSM.Event_State_Table(dictionary)
 FSM_table.print_table()
+
+FSM_state1 = FSM.State('state1', True)
+FSM_state2 = FSM.State('state2', False)
+
+FSM_test.event_state_table = FSM_table
+FSM_test.add_to_states([FSM_state1, FSM_state2])
+FSM_test.pass_starting_state()
+FSM_test.print_state_list()
+
 
 #%%
 
@@ -34,4 +41,7 @@ for ii,event in enumerate(event_list):
         # event.key
         # FSM_event.check_events(event)
         FSM_test.check_events(event)
+        FSM_test.check_table()
         print('')
+        
+
