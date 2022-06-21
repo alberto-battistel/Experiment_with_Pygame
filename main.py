1a6eea4290ae9dc10458dea275ac1eaf5313b26e
Alberto Battistel <alberto.battistel@rub.de>
2022-06-21 23:18:51 +0200

    Create an App class.

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 import pygame
 
-pygame.font.init()
+pygame.init()
 
 settings = {'screen_size': (1280, 800),
             'FPS': 60,
             }
-
-screen = pygame.display.set_mode(settings['screen_size'])
-
-clock = pygame.time.Clock()
-
-run = True
-while run:
-		clock.tick(settings['FPS'])
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				run = False
-				pygame.quit()
-				
-		pygame.display.set_caption("FPS: {:.1f}".format(clock.get_fps()))
+            
+class App:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode(settings['screen_size'])
+        self.clock = pygame.time.Clock()
+        self.running = True
+            
+    def run(self):
+        while self.running:
+            self.clock.tick(settings['FPS'])
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+                                                
+            pygame.display.set_caption("FPS: {:.1f}".format(self.clock.get_fps()))
+    pygame.quit()
+     
+if __name__ == '__main__':
+    App().run()
