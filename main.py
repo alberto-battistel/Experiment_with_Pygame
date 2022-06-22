f7cd3a8dfd3d182b59557e4cab6ca6ab55cf72cf
Alberto Battistel <alberto.battistel@rub.de>
2022-06-22 23:13:30 +0200

    Modified main.py.

--- main.py
+++ main.py
@@ -12,16 +12,24 @@ class App:
         self.screen = pygame.display.set_mode(settings['screen_size'])
         self.clock = pygame.time.Clock()
         self.running = True
-            
+    
+    def set_caption(self):
+        pygame.display.set_caption("FPS: {:.1f}".format(self.clock.get_fps()))
+    
+    def set_background(self):
+        self.screen.fill((220, 220, 220))
+                
     def run(self):
         while self.running:
+            self.real_fps = self.clock.get_fps()
             self.clock.tick(settings['FPS'])
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
-
-            self.screen.fill((220, 220, 220))
-            pygame.display.set_caption("FPS: {:.1f}".format(self.clock.get_fps()))
+            
+            self.set_background()
+            self.set_caption()
+            
     pygame.quit()
      
 if __name__ == '__main__':
