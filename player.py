9d59a4c186d1df360ad768000690c268e5e9dc0f
Alberto Battistel <alberto.battistel@rub.de>
2022-07-20 17:37:57 +0200

    Updated Player movement and physics.

--- player.py
+++ player.py
@@ -1,7 +1,9 @@
 import pygame
 from pygame.locals import *
+from pygame import Vector2 as vec
 
 from main import App
+
 
 class Sequencer():
     def __init__(self, *sprites):
@@ -46,29 +48,62 @@ class SpriteSheet():
     #        images.append(image)
         return images
 
+acceleration = 0.5
+friction = -0.12
+gravity = 0.5
+
 class Player(pygame.sprite.Sprite):
+    
     def __init__(self,  settings):
         super().__init__()
         self.rect = pygame.Rect(0, 0, 16, 16)
         self.sprites = Sequencer(*SpriteSheet("first_Experiment.png",  self.rect).get_strip())
         self.surface = self.sprites() 
-        self.rect = self.rect = self.surface.get_rect()
+        self.rect = self.surface.get_rect()
         self.settings = settings
         self.bounding_box = pygame.Rect(0, 0,  *self.settings['screen_size'])
+        self.direction = vec(0, 0)
+        self.vel = vec(0, 0)
+        self.acc = vec(0, 0)
+        self.pos = vec(0, 0)
         
-    def handle_events(self,  event):
-        if event.type == KEYDOWN:
-            if event.key == K_a:
-                self.rect.move_ip(-20, 0)
-            elif event.key == K_d:
-                self.rect.move_ip(20, 0)
-            elif event.key == K_w:
-                self.rect.move_ip(0, -20)
-            elif event.key == K_s:
-                self.rect.move_ip(0, 20)
-        self.rect.clamp_ip(self.bounding_box)
+    def handle_events(self,  _):
+        self.direction = vec(0, 0)
+        event = pygame.key.get_pressed()
         
+        if event[K_a]:
+            self.direction = vec(-1, 0)
+        elif event[K_d]:
+            self.direction = vec(1, 0)
+        elif event[K_w]:
+            self.direction = vec(0, -5)
+        elif event[K_s]:
+            self.direction = vec(0, 1)
+        
+#        if event.type == KEYDOWN:
+#            if event.key == K_a:
+#                self.direction = vec(-1, 0)
+#            elif event.key == K_d:
+#                self.direction = vec(1, 0)
+#            elif event.key == K_w:
+#                self.direction = vec(0, -1)
+#            elif event.key == K_s:
+#                self.direction = vec(0, 1)
+        
+                
+    def move(self):
+        self.acc = vec(0,  gravity)
+        self.acc += acceleration * self.direction
+        self.acc += self.vel * friction
+        self.vel += self.acc
+        self.pos += self.vel + 0.5*self.acc
+        
+        self.rect.center = self.pos
+    
     def update(self):
+        self.move()
+        self.rect.clamp_ip(self.bounding_box)
+        self.pos = self.rect.center
         self.surface = self.sprites()
         
                     
@@ -78,6 +113,7 @@ class TestRun(App):
         self.settings['FPS'] = 30
         self.player = Player(self.settings)
         self.player.settings = self.settings
+        self.player.pos = vec(*[p/2 for p in self.settings['screen_size']])
         
     def handle_events(self,event):
         self.player.handle_events(event)
