8a8ed37eb35f4a67bd3a14975bff2ca92a17dfbd
Alberto Battistel <alberto.battistel@rub.de>
2022-07-31 20:53:51 +0200

    You need to go with get_pressed for the inputs.

--- test_events.py
+++ test_events.py
@@ -1,10 +1,10 @@
 import pygame
-from pygame.locals import *
+#from pygame.locals import *
 
 pygame.init()
 
 settings = {'screen_size': (600, 400),
-            'FPS': 10,
+            'FPS': 60,
             }
             
 pygame.init()
@@ -17,22 +17,38 @@ running = True
 def set_caption():
     pygame.display.set_caption("FPS: {:.1f}".format(clock.get_fps()))
 
+keys = {pygame.K_a: lambda: print("left"),
+pygame.K_d: lambda: print("right"), 
+pygame.K_w: lambda: print("up"), 
+pygame.K_s: lambda: print("down"),
+pygame.K_SPACE: lambda: print("space"),
+            pygame.K_LEFT: lambda: print("left"), 
+            pygame.K_RIGHT: lambda: print("right"), 
+            pygame.K_UP: lambda: print("up"), 
+            pygame.K_DOWN: lambda: print("down"), }
+
+def get_inputs():
+    inputs = pygame.key.get_pressed()
+    return inputs
+    
+inputs = []
+
 while running:
     real_fps = clock.get_fps()
     
     events = pygame.event.get()
-    print("number of events: " + str(len(events)))
+#    print("number of events: " + str(len(events)))
     for event in events:
-        print(event)
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
             running = False
-#    set_caption()
-#    
-#    self.update()
-#    self.render()
-
+    
+    inputs = get_inputs()
+    for key,  value in keys.items():
+        if inputs[key]:
+            value()        
+    
     set_caption()
         
     pygame.display.flip()
