18d3405ce296f8009f20b2f2b9f46bc86c6a45b8
Alberto Battistel <alberto.battistel@rub.de>
2022-08-01 21:29:57 +0200

    Nicer blocks.

--- level.py
+++ level.py
@@ -8,21 +8,21 @@ map = [
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+            [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0,],
+            [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1,],
+            [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-            [0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0,],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1,],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,], 
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
             [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0,],
             ]
 
-color = (255,  255,  0,  255)
+color = (100,  200,  150,  255)
 rect_size = vec(32, 32)
 
 class TestWorld:
@@ -39,11 +39,14 @@ class TestWorld:
         width = self.rect_size.x*self.map_size.x
         height = self.rect_size.y*self.map_size.y
         self.surface = pygame.Surface((width, height), pygame.SRCALPHA)
-#        self.surface.fill((255, 0, 0, 128))
         
     def make_block_type(self):
         self.block = pygame.Surface(self.rect_size)
         self.block.fill(color)
+        innerer_block = pygame.Surface(self.rect_size - vec(4, 4))
+        innerer_color = tuple(min(c+50,  255) for c in color)
+        innerer_block.fill(innerer_color)
+        self.block.blit(innerer_block,  (2, 2))
         self.rects = []
     
     def generate_level(self):
